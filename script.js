7e70b15ce324cf8bc594d7e1fee114da051148bc
Stephanie Nicole Teberio <teberiostephanienicole@gmail.com>
2026-09-09 03:50:35 +0800

    Remove extra rounded footer frame

--- script.js
+++ script.js
@@ -182,6 +182,7 @@ const runPhase5Qa = () => {
     await loadScript("phase10.js");
     await loadScript("phase11.js");
     await loadScript("phase12.js");
+    await loadScript("phase13.js");
   } catch (error) {
     console.error("Lumina scripts failed to load:", error);
   }
